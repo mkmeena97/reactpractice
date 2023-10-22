eb4dcf63f748d2de49aa7f610ad0cc33ca100d9f
Mahendra <mahendrakumar27697@gmail.com>
2023-10-22 13:18:50 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Practice React
         </a>
       </header>
     </div>
